4f4f844a8c5a539744579af10785ab07389d016e
Juan Pablo Farias <juanpablo.profissional.a.f@gmail.com>
2022-10-23 18:56:41 -0300

    feat: ✨the "Lista de Clientes" page was started

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,72 @@
 import React from "react";
 import { Header } from "./components/header";
+import { MagnifyingGlass, Plus } from "phosphor-react";
+
 import "./index.css";
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div className="flex justify-center">
+      <div className="w-[1720px] p-6 pt-16">
+        <div className="flex">
+          <h1
+            className="
+            text-gray-600 text-4xl font-bold border-b-4
+            border-blue-800 
+              pb-1
+              w-full max-w-[310px]
+            "
+          >
+            Lista de Clientes
+          </h1>
+        </div>
+
+        <div
+          className="
+            flex gap-4 flex-col 
+
+            my-10 
+            
+            sm:flex-row
+            "
+        >
+          <div
+            className="
+              flex flex-1
+
+              text-xl
+
+              border-gray-400 border-[1px] rounded
+
+              p-0
+              "
+          >
+            <input
+              className="flex-1 p-3"
+              type="text"
+              placeholder="Digite o nome ou CNPJ do cliente que deseja pesquisar"
+            />
+            <button className="text-white bg-blue-800 p-3 rounded">
+              <MagnifyingGlass size={28} weight="bold" />
+            </button>
+          </div>
+
+          <button
+            className="
+              flex gap-2 items-center 
+
+            bg-blue-800 
+
+              px-5 py-4 
+
+            text-white text-xl rounded
+          "
+          >
+            <Plus size={20} weight="bold" />
+            Cadastrar cliente
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,12 +3,12 @@ function Header() {
   return (
     <header
       className="
-        p-7
+        flex items-center justify-between
+
         bg-blue-800 
         text-white 
-        flex
-        items-center
-        justify-between
+
+        p-7
     "
     >
       <div className="max-w-[58px]">
@@ -16,8 +16,8 @@ function Header() {
           <img src="/logo.png" alt="Logo da YouX Group" />
         </button>
       </div>
-      <div className="flex gap-4 items-center">
-        <div className="flex gap-2 items-center">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
           <UserCircle size={46} weight="fill" className="text-white" />
           <div>
             <h3 className="text-2xl">Juan Pablo Farias</h3>
@@ -26,7 +26,14 @@ function Header() {
         </div>
         <div>
           <button
-            className="hover:bg-slate-500 hover:rounded-full transition-all p-2 flex items-center justify-center"
+            className="
+              flex items-center justify-center
+
+              p-2
+              
+              transition-all 
+              hover:bg-slate-500 hover:rounded-full
+              "
             title="Deslogar"
           >
             <SignOut size={46} weight="fill" />
